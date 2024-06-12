2a3896acd676c635aa1c12dd4906452140ad7455
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-12 14:50:36 +0700

    feat: add product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class ProductController extends Controller {
     public function index(): View {
         $products = Product::paginate(10);
         return view('products.index', compact('products'));
+    }
+
+    public function create(): View {
+        return view('products.create');
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        Product::create([
+            'name' => $request->name,
+            'price' => $request->price,
+            'description' => $request->description,
+        ]);
+
+        return redirect()->route('products.index')->with('success', 'product is succes added');
     }
 }
